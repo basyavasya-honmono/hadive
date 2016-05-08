e65b2b1cfda2c1eddd5081c31692073c7421a40a
Cuspian <dk2926@nyu.edu>
2016-05-08 03:55:19 -0400

    tester for get_time

---
+++ get_time/get_time_test.py
@@ -0,0 +1,3 @@
+# Put the file named '2016-04-15-22-04-00_6_Ave__West_Houston_St.jpg' to the same directory for test
+from get_time import get_time
+print get_time('2016-04-15-22-04-00_6_Ave__West_Houston_St.jpg')
