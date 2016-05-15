7cb78f3ec7e5972b8668bafb3a21c26663f5bce4
priyakhokher <pnk230@nyu.edu>
2016-05-14 22:19:09 -0700

    handy functions for use

---
+++ handy/__init__.py
@@ -0,0 +1,9 @@
+
+from inputArray import rgbDict
+from inputArray import rgbList
+from inputArray import greyDict
+from inputArray import greyList
+from inputArray import showAll
+from inputArray import histogram
+
+
