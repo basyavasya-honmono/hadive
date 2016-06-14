decf73aeb55d43d76432f065cd77235e65f4da93
Bibby <bibby108@gmail.com>
2016-06-14 17:48:21 -0400

    Image fetcher sample handy

--- LabelingTool/LabelingTool.py
+++ LabelingTool/LabelingTool.py
@@ -353,7 +353,7 @@ if __name__ == '__main__':
 
         image = cursor.fetchall()
         image = image[0]
-        imgname = str(image[-3]) + str(image[2])
+        imgname = str(image[-3]) + str(image[3])
 
         img = mpimg.imread(imgname)
         # Create the canvas
