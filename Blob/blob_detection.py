1b22a9409bcbd87ffadefde107271d53eca5d800
Cuspian <dk2926@nyu.edu>
2016-06-21 05:55:33 -0400

    Returns blob centroids

--- Blob/blob_detection.py
+++ Blob/blob_detection.py
@@ -1,13 +1,37 @@
+# the current version takes in 3 consecutive images and returns the list of the blob centroids 
 import numpy as np
 import cv2
-# Script to see what blobs look like
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from scipy import ndimage
 
-
 def detector(image_list):
+	params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+	params.minThreshold = 10
+	params.maxThreshold = 200
+
+# Filter by Area.
+	params.filterByArea = True
+	params.minArea = 100
+	params.maxArea = 1000
+
+# Filter by Circularity
+	params.filterByCircularity = True
+	params.minCircularity = 0.05
+
+# Filter by Convexity
+	params.filterByConvexity = True
+	params.minConvexity = 0.05
+
+# Filter by Inertia
+	params.filterByInertia = False
+	params.minInertiaRatio = 0.01
+
+
 	edges = []
 	for i in image_list:
+
 # Loading the image in grayscale
 		img = cv2.imread(i, cv2.IMREAD_GRAYSCALE)
 
@@ -26,20 +50,35 @@ def detector(image_list):
 	differences = []
 	for i in range(1, len(edges)):
 		differences.append(abs(edges[0]-edges[i]))
-			
+	im = cv2.imread(image_list[0])
+
 # Finding the minimal values pixelwise
 	minimal = np.minimum(differences[0], differences[1])
 	#minimal = np.where(minimal>np.percentile(minimal, 98), np.percentile(minimal, 100), np.percentile(minimal,96))
-	n_blobs = 0
-
-# Joining the smaller blobs together
-	minimal = ndimage.gaussian_filter(minimal, 4)
 	
+# Joining the smaller blobs together
+	minimal = ndimage.gaussian_filter(minimal, 5)
+	mini = np.min(minimal)
+	maxi = np.max(minimal)
+
+# Scaling the brightnesses to 0..25
+	minimal = minimal / maxi * 255
+	minimal = minimal.astype(int)
+#minimal = cv2.cvtColor(minimal, cv2.CV_8U)
 # Identifying and counting the blobs
-	label_, n_blobs = ndimage.label(minimal> 50) 
+	#label_, n_blobs = ndimage.label(minimal> np.percentile(minimal, 60)) 
+
+# Detecting centroids
+	detector = cv2.SimpleBlobDetector(params)
+	keypoints = detector.detect(im)
+	centroids = []
+	
+
+	for k in keypoints:
+		centroids.append([k.pt[0], k.pt[1]])
 
 # Script to see what blobs look like	
-#	plt.imshow(minimal, cmap = 'Greys_r')
-#	plt.show()
+	#plt.imshow(minimal, cmap = 'Greys_r')
+	#plt.show()
 
-	return n_blobs
+	return centroids
