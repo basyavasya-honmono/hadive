ecf00b2063aaf1057baceb0d4183f95772e0e0c3
Bibby <bibby108@gmail.com>
2016-06-27 18:28:54 -0400

    Update LabelingTool_TwoClick.py

--- LabelingTool/LabelingTool_TwoClick.py
+++ LabelingTool/LabelingTool_TwoClick.py
@@ -414,7 +414,7 @@ if __name__ == '__main__':
         cursor.execute("""select * from images where labeled=false and id=%s limit 1;"""%(np.random.randint(68250, 177561, 1)[0]))
 
         image_fields = cursor.fetchall()
-        image_fields = image_fields[0]
+        image_fields = image_fields[2]
         imgname = str(image_fields[-5]) + str(image_fields[2])
 	imgid = image_fields[0]
 	# Closing db connections
