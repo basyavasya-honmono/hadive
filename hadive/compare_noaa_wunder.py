6420b1457b619e2b829a9cfa188801d962af3f8a
Greg Dobler <greg.dobler@gmail.com>
2017-10-30 16:18:35 -0400

    updated to compare with WU daily totals

--- hadive/compare_noaa_wunder.py
+++ hadive/compare_noaa_wunder.py
@@ -4,6 +4,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # -- read noaa
 nname = os.path.join("..", "data", "external",
@@ -12,23 +13,48 @@ noaa  = pd.read_csv(nname, parse_dates=["DATE"])
 
 # -- read weather underground and sum to date
 wname = os.path.join("..", "output", "wunder_knyc_conditions.feather")
-wund  = pd.read_feather(wname).set_index("time").resample("D").sum()
-wund.reset_index(inplace=True)
+wundr = pd.read_feather(wname)
+wund  = wundr.set_index("time").resample("D").sum()["precip"].reset_index()
+
+# -- add daily total
+wund["daily_precip"] = wundr.set_index("time").resample("D") \
+                                        .first().reset_index()["daily_precip"]
 
 # -- merge the two
 comb = pd.merge(noaa, wund, how="outer", left_on=["DATE"], right_on=["time"])
 comb.dropna(inplace=True)
 
 # -- fit a linear model
-m, b = np.polyfit(comb["PRCP"], comb["precip"], 1)
+ml, bl = np.polyfit(comb["PRCP"], comb["precip"], 1)
+mr, br = np.polyfit(comb["daily_precip"], comb["precip"], 1)
 
 # -- plot it
-ax  = comb.plot("PRCP", "precip", kind="scatter", xlim=[0, 7], ylim=[0, 7])
-lin = ax.plot(comb["PRCP"], m * comb["PRCP"] + b, color="lime",
-              label="{0} * NOAA + {1}".format(np.round(m, 3), np.round(b, 3)))
-fig = ax.get_figure()
-ax.legend(loc="center right")
-ax.set_xlabel("NOAA precipitation [inches]")
-ax.set_ylabel("Weather Underground precipitation [inches]")
+fig, [axl, axr, axb] = plt.subplots(1, 3, figsize=[10, 3.5])
+
+comb.plot("PRCP", "precip", kind="scatter", xlim=[0, 7], ylim=[0, 7], ax=axl)
+linl = axl.plot(comb["PRCP"], ml * comb["PRCP"] + bl, color="lime",
+                label="{0} * NOAA + {1}".format(np.round(ml, 3),
+                                                np.round(bl, 3)))
+fig = axl.get_figure()
+axl.legend(loc="lower right", fontsize="small")
+axl.set_xlabel("NOAA daily precipitation [inches]")
+axl.set_ylabel("WU summed precipitation [inches]")
+
+comb.plot("daily_precip", "precip", kind="scatter", xlim=[0, 7], ylim=[0, 7],
+          ax=axr, color="skyblue")
+linr = axr.plot(comb["PRCP"], mr * comb["PRCP"] + br, color="mediumpurple",
+                label="{0} * WU + {1}".format(np.round(mr, 3),
+                                              np.round(br, 3)))
+axr.legend(loc="lower right", fontsize="small")
+axr.set_xlabel("WU daily precipitation [inches]")
+axr.set_ylabel("")
+axr.set_yticklabels("")
+
+comb.plot("PRCP", "daily_precip", kind="scatter", xlim=[0, 4], ylim=[0, 4],
+          ax=axb, color="gold")
+axb.set_yticks(range(5))
+axb.set_xlabel("NOAA daily precipitation [inches]")
+axb.set_ylabel("WU daily precipitation [inches]")
+
 fig.savefig(os.path.join("..", "output", "compare_noaa_wunder.png"),
             clobber=True, facecolor=fig.get_facecolor())
