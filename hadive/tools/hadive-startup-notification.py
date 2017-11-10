96dba08f3edfce4230b566d0fb0c942e20794747
Jordan Vani <jmv423@nyu.edu>
2017-11-10 13:50:33 -0500

    Pipe stderr and stdout between child process and force memory cleanup.

--- hadive/tools/hadive-startup-notification.py
+++ hadive/tools/hadive-startup-notification.py
@@ -2,12 +2,9 @@ import os
 import sys
 import time
 import json
-import psutil
 import datetime
 import psycopg2
-import subprocess
-import pandas as pd
-from getpass import getpass
+from subprocess Popen, PIPE
 from smtplib import SMTP_SSL
 from email.header import Header
 from email.mime.text import MIMEText
@@ -44,34 +41,55 @@ def main(cmd, recipients, login, password):
         login (str): email to send notifications.
         password (str): senders password."""
 
+    # -- Write startup time and command to log.
     with open("./log.txt", "a") as f:
         f.write("{}, {}\n".format(datetime.datetime.now(), cmd))
 
-    db_prev = day = 0
-    proc = subprocess.Popen(cmd.split()) # Should quit if Popen fails.
+    db_prev = day = 0 # -- Zero db length count and day of month value.
+    # -- Run command as subprocess.
+    proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+
+    # -- While process is running continuously check status.
     while proc.poll() == None:
         # -- Get length of db.
         with psycopg2.connect("dbname='dot_pedestrian_counts'") as conn:
             with conn.cursor() as cc:
                 cc.execute("SELECT COUNT(*) FROM ped_count")
                 db_now = cc.fetchall()[0][0]
-        # -- (Active notif.) If db has grown, continue.
+
+        # -- If the db has grown set db length to current value.
         if db_now > db_prev:
             db_prev = db_now
+        # -- If the database has not grown break the while loop.
         else:
             break
-        # -- (Passive notif.) Send a daily update.
+
+        # -- Send a daily update email.
         if day != datetime.datetime.now().day:
             day = datetime.datetime.now().day
             message = ("The HaDiVe script is currently running.\n"
                        "The database now has {} records.".format(db_prev))
             for recipient in recipients.split(", "):
                 send_status_email(recipient, login, password, message)
-        time.sleep(60 * 5)
 
-    # -- While loop has broken, send notification.
+        time.sleep(60 * 5) # -- Sleep for 5 minutes.
+
+    # -- While loop has broken; the process has either died or 0 records have
+    # -- been added to the database in the last 5 minutes.
+
+    stdout, stderr = proc.communicate()
+    with open("./log.txt", "a") as f:
+        f.write("STDOUT\n")
+        f.write("{}\n".format(stdout))
+        f.write("STDERR\n")
+        f.write(s"{}\n".format(stderr))
+
+    del proc # -- Delete subprocesses object from memory to clear GPU.
+
+    # -- Send alert that the process has died.
     message = "The HaDiVe script has gone down."
     send_status_email(recipients, login, password, message)
+
     # -- Restart.
     main(cmd, recipients, login, password)
 
@@ -81,4 +99,5 @@ if __name__ == "__main__":
     with open("./secrets.json", "r") as f:
         secrets = json.load(f)
 
-    main(secrets["cmd"], secrets["recipients"], secrets["login"], secrets["password"])
+    main(secrets["cmd"], secrets["recipients"], secrets["login"],
+         secrets["password"])
